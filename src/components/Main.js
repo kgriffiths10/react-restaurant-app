e595b410fce93a09239d53843150c0e279811e08
Katya Griffiths-Julien <KGRIFFITHS20@ubishops.ca>
2024-10-02 13:52:14 -0400

    added components

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <main>
+            <h1>Welcome to Pinchocchio</h1>
+            <p>Reserve your table at our resturant today!</p>
+        </main>
+    );
+}
+
+export default Main;
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Nav(){
+    return (
+        <nav>
+            <ul>
+                <li><a href="/home">Home</a></li>
+                <li><a href="/menu">Menu</a></li>
+                <li><a href="/reserve">Reserve a Table</a></li>
+                <li><a href="/contact">Contact Us</a></li>
+
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
